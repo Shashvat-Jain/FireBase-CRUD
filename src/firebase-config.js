12ddc8268c20e45625d21d3550b857efd6a1b0e2
shashvat_sj <20je0897@mc.iitism.ac.in>
2023-09-23 15:16:23 +0530

    workflow added again

--- src/firebase-config.js
+++ src/firebase-config.js
@@ -2,13 +2,13 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyD7ZO7QWEgrCDh1HKwYBt8cPqBOs8gk9l8",
-  authDomain: "react-crud-28efe.firebaseapp.com",
-  projectId: "react-crud-28efe",
-  storageBucket: "react-crud-28efe.appspot.com",
-  messagingSenderId: "759735099836",
-  appId: "1:759735099836:web:8b9018b691efeb3e350bb9",
-  measurementId: "G-Y647WMMVL4"
+  apiKey: "YOUR ID HERE",
+  authDomain: "YOUR ID HERE",
+  projectId: "YOUR ID HERE",
+  storageBucket: "YOUR ID HERE",
+  messagingSenderId: "YOUR ID HERE",
+  appId: "YOUR ID HERE",
+  measurementId: "YOUR ID HERE"
 };
 
 const app = initializeApp(firebaseConfig);
